d4810535a3faa037824e7b1f893b26e017d3694e
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-03-27 17:35:03 +0900

    データベース生成、postsテーブルマイグレーション

---
+++ database/migrations/2021_03_27_082410_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    public function boot() {
+        Schema::defaultStringLength(191);
+        if(\App::environment('production')) {
+            \URL::forceSchema('https');
+        }
+    }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('age');
+            $table->integer('sex');
+            $table->integer('request');
+            $table->text('memo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
