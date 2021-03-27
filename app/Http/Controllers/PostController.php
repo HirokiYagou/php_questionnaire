d6a119d93991fcf8037697bf2f42760de115b22c
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-03-27 17:48:47 +0900

    postコントローラー生成

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    //
+}
